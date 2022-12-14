3b075fa1fe2edb41ed481ea093f1a577dcef570a
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2022-12-14 19:22:07 +0530

    Payment Page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import MainNavbar from './components/Navbar/MainNavbar';
+import Thnks from './components/Payment/paymentComponent/Thnks';
 
 function App() {
   return (
    <div className='App'>
     <MainNavbar />
+    
+    <Thnks/>
     <Footer />
    </div>
   );
---
+++ src/components/Payment/paymentComponent/Thnks.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Alert,AlertIcon,AlertDescription,AlertTitle} from "@chakra-ui/react"
+function Thnks() {
+  return (
+    <div>
+
+
+<Alert
+  status='success'
+  variant='subtle'
+  flexDirection='column'
+  alignItems='center'
+  justifyContent='center'
+  textAlign='center'
+  height='200px'
+>
+  <AlertIcon boxSize='40px' mr={0} />
+  <AlertTitle mt={4} mb={1} fontSize='lg'>
+    
+Payment Done!
+  </AlertTitle>
+  <AlertDescription maxWidth='sm'>
+  Thank you for your purchase!
+  </AlertDescription>
+</Alert>
+    </div>
+  )
+}
+
+export default Thnks
+
