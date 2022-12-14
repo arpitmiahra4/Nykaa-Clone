8e063b5cc1698765c752dabad1b7897a4fc05d88
arpitmiahra4 <arpitmiahra4@gmail.com>
2022-12-14 13:36:17 +0530

    adding footer module css and footer.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
 import MainNavbar from './components/Navbar/MainNavbar';
 
 function App() {
   return (
    <div className='App'>
     <MainNavbar />
+    <Footer />
    </div>
   );
 }
