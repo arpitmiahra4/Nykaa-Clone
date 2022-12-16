c7c99a786fba8c07f096ac8c960fe59f89595541
arpitmiahra4 <arpitmiahra4@gmail.com>
2022-12-17 04:20:59 +0530

    adding route

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Products from './components/ProductPage/Products'
 import MAIN from './components/Profile/Main'
+import HomePage from './components/Home/HomePage'
 import SingleProduct from './components/SingleProduct/SingleProduct'
 
 const Main = () => {
   return(
     <Routes>
+    <Route path="/" element={<HomePage />} />
         <Route path="/profile/*" element={<MAIN />} />
         <Route path='/products' element={<Products/>}></Route>
         <Route path='/products/:id' element={<SingleProduct/>}></Route>
