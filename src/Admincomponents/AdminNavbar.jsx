336867258c78acb3fbac5de9edd84111b5dcbbe1
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2022-12-17 09:08:05 +0530

    AdminFooter rearragemet done

--- src/Admincomponents/AdminNavbar.jsx
+++ src/Admincomponents/AdminNavbar.jsx
@@ -5,15 +5,3 @@
 
 
 
-import React from "react";
-
-const AdminFooter = () => {
-  return (
-    <div className="bg-dark text-white text-center p-4 mt-3">
-      <h2>Design And Developed By : Chandrashekhar joshi</h2>
-      <h6>All Rights Reserved &copy; Chandrashekhar Joshi </h6>
-    </div>
-  );
-};
-
-export default AdminFooter;
