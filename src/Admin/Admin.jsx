5e787d8e591ff48b871c7aa6ca12ce22b9238f0b
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2022-12-17 10:46:34 +0530

    Admin page setup

--- src/Admin/Admin.jsx
+++ src/Admin/Admin.jsx
@@ -1,11 +1,13 @@
+
+
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AdminLoginPage from "./Pages/LoginPage";
-import AdminUsersList from "./Pages/UsersList";
-import AdminHomePage from "./Pages/HomePage";
-import AdminFooter from "./Components/Footer";
+import AdminLoginPage from './AdminPages/AdminLoginPage'
+import AdminUsersList from "./AdminPages/AdminUsersList";
+import AdminHomePage from "./AdminPages/AdminHomePage";
+import AdminFooter from "./Admincomponents/AdminFooter";
 function Admin() {
   return (
     <>
@@ -22,7 +24,7 @@ function Admin() {
       />
       <Routes>
         <Route path="/" element={<AdminLoginPage />} />
-        <Route path="/profile" element={<AdminHomePage />} />
+        <Route path="/profile" element={<AdminHomePage/>} />
         <Route path="/users" element={<AdminUsersList />} />
       </Routes>
       <AdminFooter />
