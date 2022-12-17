d431990e5e0aaa1065227e031affcd70529e1cc3
AJ31032003 <arihantj673@gmail.com>
2022-12-17 11:29:46 +0530

    fp06_169day5

--- src/components/Profile/Main.jsx
+++ src/components/Profile/Main.jsx
@@ -5,7 +5,7 @@ import ProfileOption from "./ProfileOption"
 
 const MAIN = () => {
   return (
-    <div style={{display:"flex"}}>
+    <div style={{display:"flex",width:"80%",margin:"auto",marginTop:"20px",marginBottom:"40px"}}>
       <Box w="250px" boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" >
         <ProfileOption />
       </Box>
