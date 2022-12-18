36f73ab33c759a24c1f3844c9ff7a462ed3efd78
arpitmiahra4 <arpitmiahra4@gmail.com>
2022-12-18 13:16:02 +0530

    adding productCard styles

--- src/components/ProductPage/ProductCard.jsx
+++ src/components/ProductPage/ProductCard.jsx
@@ -1,60 +1,72 @@
-import { useToast } from '@chakra-ui/react';
-import React, { useState } from 'react'
-import { BsHeart } from 'react-icons/bs';
-import { FaStar } from 'react-icons/fa';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { addToCart } from '../../actions/cart';
-import { addToWishlist } from '../../actions/wishlist';
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { BsHeart } from "react-icons/bs";
+import { FaStar } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addToCart } from "../../actions/cart";
 import styles from "../ProductPage/products.module.css";
-
+import { useToast } from "@chakra-ui/react";
 const ProductCard = ({ product }) => {
   const [stars, setStars] = useState(Number(product.rating));
-
-  const dispatch=useDispatch();
-  const data=useSelector((state)=>state);
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state);
   console.log(data);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const starArr = new Array(Math.floor(stars)).fill(0);
   const handleClick = (id) => {
-     navigate(`/products/${id}`) ;
-    
-  }
+    navigate(`/products/${id}`);
+  };
 
-  const handleCart=(product)=>{
-       dispatch(addToCart(product));
-
-  }
-
-  const handleWishList=(product)=>{
-    dispatch( addToWishlist(product));
-
-}
- 
-
+  const handleCart = (product) => {
+    toast({
+      title: "Item Added To Bag",
+      description: "Added Successfully.",
+      status: "success",
+      duration: 4000,
+      isClosable: true,
+    });
+    dispatch(addToCart(product));
+  };
+  const toast = useToast();
   return (
-    <div className={styles.productCard} >
-      <img src={product.image1} alt={product.title} onClick={() => handleClick(product.id)}/>
-      <p className={styles.title}>{product.title}</p>
-      <p className={styles.price}>MRP : <span style={
-        { "textDecoration": "line-through", "marginRight": "8px" }
-      }>₹{product.price} </span> <span className={styles.title}>₹{product.off_price}</span> | <span style={{ "color": "green", "fontWeight": "medium" }}>{product.offer}% OFF</span></p>
+    <Box className={styles.productCard} w="300px" h="445px">
+      <Image
+        src={product.image1}
+        alt={product.title}
+        onClick={() => handleClick(product.id)}
+        w="250px"
+      />
+      <Box>
+        <Text h="80px" textAlign={"center"}>
+          {product.title}
+        </Text>
+        <Text textAlign={"center"}>
+          MRP :{" "}
+          <span style={{ textDecoration: "line-through", marginRight: "8px" }}>
+            ₹{product.price}{" "}
+          </span>{" "}
+          <span className={styles.title}>₹{product.off_price}</span> |{" "}
+          <span style={{ color: "green", fontWeight: "medium" }}>
+            {product.offer}% OFF
+          </span>
+        </Text>
+      </Box>
+      <Box className={styles.stars}>
+        {starArr?.map((e) => {
+          return <FaStar />;
+        })}
+      </Box>
+      <Box className={styles.btns}>
+        <button className={styles.heartbtn}>
+          <BsHeart />
+        </button>
+        <button className={styles.primary} onClick={() => handleCart(product)}>
+          Add To Bag
+        </button>
+      </Box>
+    </Box>
+  );
+};
 
-      <div className={styles.stars}>
-        {
-          starArr?.map((ele) => {
-            return <FaStar />
-          })
-
-        }
-      </div>
-
-      <div className={styles.btns}>
-        <button className={styles.heartbtn} onClick={()=>handleWishList(product)}><BsHeart /></button>
-        <button className={styles.primary} onClick={()=>handleCart(product)}>Add to Bag</button>
-      </div>
-    </div>
-  )
-}
-
-export default ProductCard
+export default ProductCard;
